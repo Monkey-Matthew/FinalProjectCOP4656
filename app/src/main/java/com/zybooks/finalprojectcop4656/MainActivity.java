0b686276c49d94d74c1d89f132ed21cd653752ab
Matthew-Gonzales <monkeymatthew0217@gmail.com>
2024-04-26 16:04:01 -0400

    First Commit

---
+++ app/src/main/java/com/zybooks/finalprojectcop4656/MainActivity.java
@@ -0,0 +1,14 @@
+package com.zybooks.finalprojectcop4656;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
